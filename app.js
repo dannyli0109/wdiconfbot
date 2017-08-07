3dbc5c9825fb427a43757f438ea49d74ed1fd59b
Danny Li <dannyli0109@gmail.com>
2017-08-07 19:13:43 +1000

    test

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("danny said: %s", session.message.text);
+    session.send("hi said: %s", session.message.text);
 });
