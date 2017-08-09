0061d122b59c3f315f12d7093ad80cf54277bef1
Danny Li <dannyli0109@gmail.com>
2017-08-09 16:43:10 +1000

    apis

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require("request")
 
 // Setup Restify Server
 //LEdaFjQaXAeP7xfXyGXLQ71
@@ -43,17 +44,82 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, [
 
+
   function (session) {
-    session.userData.name = session.message.user.name;
-    session.send("Welcome to wdi conf voting system!")
-    builder.Prompts.choice(session, "What is your first choice?", topics);
-  },
-  function (session, results) {
-    session.userData.first = results.response.entity;
-    builder.Prompts.choice(session, "What is your second choice?", topics);
-  },
-  function (session, results) {
-    session.userData.second = results.response.entity;
-    session.send("Got it... " + session.userData.name + " chooses " + session.userData.first + " and " + session.userData.second);
+    session.send("Sorry i don't understand, but you can ask Kasun.")
   }
+  // function (session) {
+  //   session.userData.name = session.message.user.name;
+  //   session.send("Welcome to wdi conf voting system!")
+  //   builder.Prompts.choice(session, "What is your first choice?", topics);
+  // }
+  // ,
+  // function (session, results) {
+  //   session.userData.first = results.response.entity;
+  //   builder.Prompts.choice(session, "What is your second choice?", topics);
+  // },
+  // function (session, results) {
+  //   session.userData.second = results.response.entity;
+  //   session.send("Got it... " + session.userData.name + " chose " + session.userData.first + " and " + session.userData.second);
+  // }
 ]);
+
+function sendProactiveMessage(address) {
+  var msg = new builder.Message().address(address);
+  msg.text('Hello, this is a notification');
+  msg.textLocale('en-US');
+  bot.send(msg);
+}
+
+bot.recognizer({
+  recognize: function (context, done) {
+    var intent = { score: 0.0 };
+
+    if (context.message.text) {
+      if (context.message.text.toLowerCase().indexOf('hi') > -1 ||
+      context.message.text.toLowerCase().indexOf('hello') > -1 ||
+      context.message.text.toLowerCase().indexOf('hey') > -1) {
+        intent = { score: 1.0, intent: 'Hello' };
+      }
+
+      if (context.message.text.toLowerCase().indexOf('help') > -1) {
+        intent = { score: 1.0, intent: 'Help' };
+      }
+      if (context.message.text.toLowerCase().indexOf('goodbye') > -1) {
+        intent = { score: 1.0, intent: 'Goodbye' };
+      }
+    }
+    done(null, intent);
+  }
+});
+
+// Add a help dialog with a trigger action that is bound to the 'Help' intent
+bot.dialog('helpDialog', function (session) {
+  session.endDialog("This bot will echo back anything you say. Say 'goodbye' to quit.");
+}).triggerAction({ matches: 'Help' });
+
+
+// Add a global endConversation() action that is bound to the 'Goodbye' intent
+bot.endConversationAction('goodbyeAction', "Ok... See you later.", { matches: 'Goodbye' });
+
+
+bot.dialog('helloDialog', [
+  function(session) {
+    builder.Prompts.text(session, 'Hi! What is your name?');
+  },
+  function (session, results) {
+    request({
+      uri: "http://localhost:3000/api/users",
+      qs: {
+        name: results.response
+      }
+    }, function(error, res, body) {
+        var user = JSON.parse(body)
+        session.userData.name = user.name
+        session.userData.id = user.id
+        session.send("Hello, " + session.userData.name + "! Welcome to wdi conf 2017 voting system." + "Id: " + session.userData.id);
+        session.send("How can I help you today?");
+
+    })
+  }
+]).triggerAction({ matches: 'Hello' });
